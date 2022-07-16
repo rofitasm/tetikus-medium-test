aa723ca9f0ebe4df87d854464373a45dc99afbf0
GDN\rofita.musdalifah <rofita.musdalifah@gdn-commerce.com>
2022-07-16 22:25:15 +0700

    add search article scenario

--- src/main/java/com/github/ubaifadhli/pages/medium/HomePage.java
+++ src/main/java/com/github/ubaifadhli/pages/medium/HomePage.java
@@ -2,8 +2,12 @@ package com.github.ubaifadhli.pages.medium;
 
 import com.github.ubaifadhli.annotations.BaseURL;
 import com.github.ubaifadhli.annotations.Locator;
+import com.github.ubaifadhli.annotations.MobileLocator;
+import com.github.ubaifadhli.annotations.WebLocator;
 import com.github.ubaifadhli.pages.PageObject;
 import com.github.ubaifadhli.util.Element;
+import com.github.ubaifadhli.util.MobileElementFunction;
+import com.github.ubaifadhli.util.SwipeDirection;
 
 @BaseURL("https://www.medium.com")
 public class HomePage extends PageObject {
@@ -15,8 +19,35 @@ public class HomePage extends PageObject {
             mobileID = "com.medium.reader:id/sign_in_twitter_button")
     private Element signInWithTwitterButton;
 
+    @MobileLocator(accessibilityID = "Discover something new")
+    private Element searchButton;
+
+    @Locator(webXPath = "//input[@aria-label='search']",
+            mobileID = "com.medium.reader:id/search_input")
+    private Element searchInput;
+
+    @Locator(webXPath = "//a[@aria-label='Post Preview Title']//h2",
+            mobileXPath = "//android.widget.TextView[@resource-id='com.medium.reader:id/post_preview_title']")
+    private Element searchResultTitle;
+
+
     public void goToTwitterLoginPage() {
         signInButton.waitUntilClickable().click();
         signInWithTwitterButton.waitUntilClickable().click();
     }
+
+    public void searchForArticle(String keyword) {
+        searchButton.waitUntilClickable().click();
+
+        searchInput.waitUntilClickable().click();
+
+        searchInput.waitUntilClickable().typeIntoField(keyword);
+
+        searchInput.pressEnter();
+    }
+
+    public String getFirstArticleTitle() {
+        return searchResultTitle.waitUntilVisible().getText();
+    }
+
 }
--- src/test/java/TetikusCoreRunner.java
+++ src/test/java/TetikusCoreRunner.java
@@ -3,6 +3,9 @@ import com.github.ubaifadhli.runners.TetikusBaseRunner;
 import com.github.ubaifadhli.util.*;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class TetikusCoreRunner extends TetikusBaseRunner {
     private HomePage homePage;
     private LoginPage loginPage;
@@ -18,5 +21,16 @@ public class TetikusCoreRunner extends TetikusBaseRunner {
 
         loginPage.fillTwitterLoginCredentials(username, password);
         loginPage.waitForHomeTitle();
+    }
+    @Test
+    public void searchForArticle() {
+        String SEARCH_KEYWORD = "spring boot";
+        String EXPECTED_FIRST_ARTICLE_NAME = "How to scale Microservices with Message Queues, Spring Boot, and Kubernetes";
+
+        homePage.openPage();
+
+        homePage.searchForArticle(SEARCH_KEYWORD);
+
+        assertThat(homePage.getFirstArticleTitle(), equalTo(EXPECTED_FIRST_ARTICLE_NAME));
     }
 }
